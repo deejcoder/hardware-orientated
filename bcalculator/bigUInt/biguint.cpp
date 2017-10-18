78eefc31472663b68b1e4e267de30d280eb36e87
iiNzTicTx <dylant@live.co.uk>
2017-10-18 21:10:19 +1300

    assign5-update

--- bcalculator/bigUInt/biguint.cpp
+++ bcalculator/bigUInt/biguint.cpp
@@ -15,7 +15,7 @@ bigUInt::bigUInt() {
 bigUInt::bigUInt(unsigned int n) {
 
     string nstr = to_string(n);
-    this->p = new char[BIGINT_SIZE]();
+    this->p = new char[nstr.length() + 1]();
     
 
     //for all values of size_t, size_t >= 0 since unsigned, end at 0 else breaking point
@@ -28,10 +28,15 @@ bigUInt::bigUInt(unsigned int n) {
 
 bigUInt::bigUInt(const char *s) {
     size_t len = strlen(s);
-    this->p = new char[BIGINT_SIZE]();
+    this->p = new char[len + 1]();
 
     int counter = 0;
+    bool nonzero = false;
     for (size_t i = len; i-- > 0;) {
+
+        //skip leading zeros
+        if (!nonzero && s[i] == '0') continue;
+
         this->p[counter] = s[i];
         counter++;
     }
@@ -39,7 +44,7 @@ bigUInt::bigUInt(const char *s) {
 
 bigUInt::bigUInt(const bigUInt &x) {
     size_t len = strlen(x.get_p());
-    this->p = new char[BIGINT_SIZE]();
+    this->p = new char[len + 1]();
     //strcpy(this->p, x.get_p());
 }
 
@@ -50,43 +55,73 @@ bigUInt::~bigUInt() {
 }
 
 void bigUInt::add(unsigned int n) {
+    printf("n: %d\n", n);
     for (int i = n; i-- > 0;) {
         this->increment();
     }
 }
 
 void bigUInt::add(const bigUInt &x) {
+    int multiplier = 1;
+    int value;
 
+    for (int i = 0; i < strlen(x.p); i++) {
+
+        value = x.p[i] - '0'; //char as int
+        this->add(value*multiplier);
+        multiplier *= 10;
+    }
 }
 
 void bigUInt::increment() {
-    char num = this->get_p()[0]; int index = 0;
+    int index = 0;
+    int len = strlen(this->p);
     
+    //since the number is in reverse, find the first cell with a non-9
+    char num = this->p[0];
     while (num == '9') {
-        if (index >= strlen(this->get_p())) break;
-        num = this->get_p()[index];
+        if (index > len) {
+            index++; //set it to > len
+            break;
+        }
+            
+        num = this->p[index];
         index++;
     }
+
+    //case 1: if the first cell is < 9
     if (index == 0) {
         this->p[index]++;
     }
-    else if(index != strlen(this->get_p())) {
 
+    //case 2: if all digits are 999, add one @ end & set all else to 0
+    if(index > len) {
 
+        //adding one cell & set all cells to 0, assuring \0 @ end
+        len++;
+        char* temp = new char[len + 1]();
+        memset(temp, '0', len);
+
+        //set most significant to 1
+        temp[len - 1] = '1';
+
+        delete[](this->p);
+        this->p = temp;
+    }
+
+    //case 3: increase the non-9 by one, set all before it to 0
+    else {
         index--;
         this->p[index]++;
         for (; index-- > 0;) {
             this->p[index] = '0';
         }
     }
-    else {
-        //...set last to one, add one at start (in reverse)
-
-    }
+    printf("\nthis: %s, length: %d, %d", this->p, len, strlen(this->p));
 }
 
 void bigUInt::print() {
-    const size_t len = BIGINT_SIZE;
+    const size_t len = strlen(this->get_p());
 
     for (size_t i = len; i-- > 0;) {
         cout << this->get_p()[i];
--- bcalculator/bigUInt/main.cpp
+++ bcalculator/bigUInt/main.cpp
@@ -5,14 +5,19 @@ using namespace std;
 
 int main()
 {
-    bigUInt* a = new bigUInt(2);
-    a->add(20);
+    bigUInt* a = new bigUInt(199);
     a->print();
     a->increment();
-    a->print();
+    a->add(100);
     a->print();
     bigUInt* b = new bigUInt("099999999999999999999");
     b->print();
+    b->increment();
+    b->print();
+    bigUInt* c = new bigUInt("123");
+    c->print();
+    c->add(*a);
+    c->print();
     delete a;
     delete b;
     while (1);
